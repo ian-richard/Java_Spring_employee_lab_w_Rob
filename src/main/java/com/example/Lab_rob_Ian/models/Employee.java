b549c138c053f21db73c1763daecdfb7a387a117
Ian F <ian.fiddeman@gmail.com>
2021-01-18 14:47:28 +0000

    initial commit

---
+++ src/main/java/com/example/Lab_rob_Ian/models/Employee.java
@@ -0,0 +1,90 @@
+package com.example.Lab_rob_Ian.models;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "employeeNumber")
+    private int employeeNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    public Employee(){
+
+    }
+
+    public Employee(Long id, String firstName, String lastName, int age, int employeeNumber, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.employeeNumber = employeeNumber;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
